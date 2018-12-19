4b6bb99a76dbca713946eb4ebed169e3797eb8d5
马可 <424445024@qq.com>
2018-12-19 15:47:31 +0800

    reset

--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import fastClick from 'fastclick'
+import './assets/style/reset.css'
+import './assets/style/border.css'
 Vue.config.productionTip = false
+fastClick.attach(document.body);//快速点击，移动端click有300ms延迟
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/index.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+{{msg}}
+</div>
+</template>
+<script>
+export default{
+  data () {
+    return {
+      msg: 'hello Vue'
+    }
+  }
+}
+</script>
+<style>
+</style>
