8d20367d85fa5598d2bc285629e3f33f38f7c93c
马可 <424445024@qq.com>
2018-12-21 14:12:58 +0800

    list

--- src/pages/city/city.vue
+++ src/pages/city/city.vue
@@ -2,20 +2,24 @@
 	<div class="city">
 		<city-header></city-header>
 		<city-search></city-search>
+		<city-list></city-list>
 	</div>
 </template>
 
 <style lang='stylus' scoped>
 @import '~style/varibles.styl'
+.city
+	background: #eee
 </style>
 
 <script>
 import cityHeader from './components/cityheader'
 import citySearch from './components/search'
+import cityList from './components/list'
 	export default{
 		name: 'city',
 		components:{
-			cityHeader,citySearch
+			cityHeader,citySearch,cityList
 		}
 	}
 </script>
---
+++ src/pages/city/components/list.vue
@@ -0,0 +1,52 @@
+<template>
+	<div class="list">
+		<div class="area">
+			<div class="title ">当前城市</div>
+			<div class="container">
+				<div class='container-item'>北京</div>
+				<div class='container-item'>北京</div>
+				<div class='container-item'>北京</div>
+				<div class='container-item'>北京</div>
+				<div class='container-item'>北京</div>
+			</div>
+		</div>
+		<div class="area">
+			<div class="title ">热门城市</div>
+			<div class="container ">
+				<div class='container-item'>北京</div>
+			</div>
+		</div>
+		<div class="area">
+			<div class="title ">A</div>
+			<div class="container">
+				<div class="container-item">北京</div>
+			</div>
+		</div>
+	</div>
+</template>
+<style lang='stylus' scoped>
+@import '~style/varibles.styl'
+.list
+	background: #eee
+	.area
+		.title
+			font-size: .35rem
+			line-height: .50rem
+			color: rgba(0,0,0,.8)
+			padding: .3rem .2rem
+		.container
+			overflow: hidden
+			.container-item
+				text-align:center
+				float: left
+				width: 33.33%
+				border: .01rem solid #eee
+				box-sizing: border-box
+				line-height: .9rem
+				background: white
+</style>
+<script>
+	export default{
+		name:'list'
+	}
+</script>
